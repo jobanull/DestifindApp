30fe74e454357561dc11f5ab2cf00669fcaf98df
Ikhdanul Widad Joban <72365147+zyandaru@users.noreply.github.com>
2023-12-14 22:01:02 +0700

    Revise Login Result

--- MobileDevelopment/app/src/main/java/com/example/mobiledevelopment/data/pref/LoginResult.kt
+++ MobileDevelopment/app/src/main/java/com/example/mobiledevelopment/data/pref/LoginResult.kt
@@ -1,19 +1,14 @@
 package com.example.mobiledevelopment.data.pref
 
-import com.google.gson.annotations.SerializedName
-
 data class LoginResult(
 
-    @field:SerializedName("email")
-    val email: String? = null,
+    val email: String? = "",
 
-    @field:SerializedName("token")
-    val token: String? = null,
+    val token: String? = "",
 
-    val age: Int = 1
-    ,
+    val age: Int = 1,
+
     val category: String = "",
-
 
     val isLogin: Boolean = false
 )
